49cecc0a75f7cc879d1d8073c629b5ddfe9b9a89
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-19 07:10:39 +0000

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+"""
+A welcoming message for your Flask application!
+"""
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+getting flask proficiency
