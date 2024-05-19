94179a0526c05a520f461d1152917651f1a67e46
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-19 13:13:10 +0300

    new

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,9 +27,9 @@ def c_text(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
+def python_text(text='is cool'):
     """Returns python text"""
-    return 'Python s' + text.replace('_', ' ')
+    return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
