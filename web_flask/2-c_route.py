eb1a35a3b3025a6836ec9cc8b80a1f611cbe1535
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-19 12:32:17 +0300

    ficed inclusion of text issues

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Outputs the script: Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """returns HBNB only wothout hello"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Returns assigned C text"""
+    return 'C ' + text.replace('_', ' ') # type: ignore
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
