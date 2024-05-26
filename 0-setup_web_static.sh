a3de60a5f113153cb001d636d313e91c070ac01f
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-05-26 15:24:27 +0300

    new

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,60 +1,38 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+# Server for  deployment of web_static.
 
-# Install Nginx if it is not already installed
-if ! which nginx > /dev/null; then
-    echo "Nginx is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install -y nginx
-else
-    echo "Nginx is already installed."
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create the required directories
-directories=(
-    "/data"
-    "/data/web_static"
-    "/data/web_static/releases"
-    "/data/web_static/shared"
-    "/data/web_static/releases/test"
-)
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-for dir in "${directories[@]}"; do
-    if [ ! -d "$dir" ]; then
-        echo "Creating directory $dir..."
-        sudo mkdir -p "$dir"
-    else
-        echo "Directory $dir already exists."
-    fi
-done
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create a fake HTML file
-echo "Creating fake HTML file..."
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Welcome to techpackets.tech
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Create (or recreate) the symbolic link
-if [ -L "/data/web_static/current" ]; then
-    echo "Removing existing symbolic link..."
-    sudo rm -f /data/web_static/current
-fi
-echo "Creating new symbolic link..."
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# Give ownership of the /data/ folder to the ubuntu user AND group
-echo "Changing ownership of /data/ to ubuntu user and group..."
-sudo chown -R ubuntu:ubuntu /data/
+    location /redirect_me {
+        return 301 techpackets.tech/;
+    }
 
-# Update the Nginx configuration
-echo "Updating Nginx configuration..."
-sudo sed -i '/^\tlocation \/ {/i \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Restart Nginx to apply the changes
-echo "Restarting Nginx..."
-sudo service nginx restart
-
-echo "Setup completed successfully."
+service nginx restart
